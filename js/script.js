f0530e8086d57e30ebfd6c23cf60c8f70ea6c006
Greco <sydney.greco@uleth.ca>
2018-04-09 12:45:57 -0600

    height values edited styling to come

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onload = init;
 
 //function init() {
     function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 49.69349, lng: -112.84184};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
